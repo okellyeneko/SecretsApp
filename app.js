ece013a2c312a2130e41e3dbfe0b359d32cdeb7e
Eneko O'Kelly <okelly.eneko@gmail.com>
2023-01-28 16:33:26 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const mongoose = require('mongoose')
+const encrypt = require('mongoose-encryption')
+const app = express()
+require('dotenv').config()
+
+mongoose.set('strictQuery', false)
+mongoose.connect("mongodb://0.0.0.0:27017/usersDB")
+
+console.log(process.env.SECRET)
+
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+app.use(bodyParser.urlencoded({extended: true}))
+
+const userSchema = new mongoose.Schema ({
+    email: {type: String},
+    password:  {type: String}
+})
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+
+const User = mongoose.model('User', userSchema)
+
+
+app.get('/', function(req, res){
+    res.render("home")
+})
+
+app.get('/login', function(req, res){
+    res.render("login")
+})
+
+app.get('/register', function(req, res){
+    res.render("register")
+})
+
+app.post('/register', function(req, res){
+    const username = req.body.username
+    const password = req.body.password
+    const newUser = new User ({
+        email: username,
+        password: password
+    })
+    newUser.save(function(err){
+        if(!err){
+            res.render("secrets")
+        } else {
+            console.log(err)
+        }
+    })
+})
+
+app.post('/login', function(req, res){
+    const username = req.body.username
+    const password = req.body.password
+
+    User.findOne({email: username}, function(err, foundUser){
+        if(err){
+            console.log(err)
+        } else {
+            if (foundUser){
+                if (foundUser.password === password)
+                res.render("secrets")
+            }
+        }
+    })
+})
+
+
+app.listen(3000, function(){
+    console.log('Server up and running on port 3000')
+})
